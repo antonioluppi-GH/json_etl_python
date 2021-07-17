dda99ff266ab9f6d73c785f662949933029a7a9d
antonioluppi-GH <83036973+antonioluppi-GH@users.noreply.github.com>
2021-07-17 09:23:41 -0300

    Update Ejecicio_Profundización_Json.py

--- Ejecicio_Profundización_Json.py
+++ Ejecicio_Profundización_Json.py
@@ -4,19 +4,20 @@ import requests
 
 import matplotlib.pyplot as plt
 
+from requests.api import get
+
 url = 'https://api.mercadolibre.com/sites/MLA/search?category=MLA1459&q=Departamentos%20Alquilers%20Mendoza%20&limit=50'
 
 response = requests.get("https://api.mercadolibre.com/sites/MLA/search?category=MLA1459&q=Departamentos%20Alquilers%20Mendoza%20&limit=50")
 
 json_response = response.json()
 
-json_response["results"]
-
-
 
 def fetch():
-    dataset = [[["price"], ["condition"]] for x in json_response["results"] if x["currency_id"] == "ARS"]
-    print(dataset)
+    dataset = [{'price':x.get("price"), 'condition':x.get("condition")} for x in json_response["results"]
+                 if x["currency_id"] == "ARS"]
+
+    
     return dataset
 
 
